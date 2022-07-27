2f3e4913d20462d42ceb9ba094f8cf1278f158c1
Letícia Amaral <leticiaamarallara@hotmail.com>
2022-07-27 16:51:42 -0300

    primeiro commit

---
+++ jsm_projeto/jsm_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JsmAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'jsm_app'
---
+++ jsm_projeto/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for jsm_projeto project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jsm_projeto.settings')
+
+application = get_wsgi_application()
