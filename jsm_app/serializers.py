735fffacfa4e6b1a13982ab3ec57cae630857567
RaquelGomes25 <92063008+RaquelGomes25@users.noreply.github.com>
2022-08-14 20:53:16 -0300

    Criado endpoint da API  para Produto e Usuario

--- jsm_app/serializers.py
+++ jsm_app/serializers.py
@@ -1,10 +1,27 @@
 from dataclasses import fields
 from rest_framework import serializers
-from .models import Pedido
+from .models import Pedido, Usuario, Produto
+
 
 class PedidoSerializer(serializers.ModelSerializer):
 
     class Meta:
 
         model = Pedido
+        fields = '__all__'
+
+
+class UsuarioSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Usuario
+        fields = '__all__'
+
+
+class ProdutoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+    
+        model = Produto
         fields = '__all__'
